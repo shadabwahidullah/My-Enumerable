26af87f6b8c2aad3f924ce8f2d0bcb6d116cbfe6
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-17 23:20:24 +0430

    Add module and it's methods

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,13 +1,23 @@
 module MyEnumerable
-    def MyEnumerable.all
-
+    def all?
+        @list.each do |e|
+             return false unless yield (e)
+        end
+        true
     end
 
-    def MyEnumerable.any
-        
+    def any?
+        @list.each do |e| 
+            return true if yield (e)
+        end
+        false
     end
 
-    def MyEnumerable.filter
-        
+    def filter
+        filtered = []
+        @list.each do |e| 
+            filtered.push(e) if yield (e)
+        end
+        filtered
     end
 end
--- MyList.rb
+++ MyList.rb
@@ -1,5 +1,14 @@
+require_relative 'MyEnumerable'
+
 class MyList
-    def intialize(list)
-        @list = list
+    include MyEnumerable
+    def initialize(*args)
+        @list = args
     end
 end
+
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+print(list.filter(&:even?))
